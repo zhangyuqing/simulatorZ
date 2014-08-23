0d9b7d2a0ec84df282fc83a9ee8f2a0bedd76235
Yuqing Zhang <zhangyuqing.pkusms@gmail.com>
2014-08-23 22:53:41 +0800

    change geneFilter

--- R/geneFilter.R
+++ R/geneFilter.R
@@ -2,29 +2,31 @@ geneFilter <- structure(function
 ### the function to filter genes by intergrative correlation
 (esets,
  ### a list of Expression Sets with same number of observations and genes 
- cor.cutoff=0.6 
+ cor.cutoff=0.5 
  ### the cutoff threshold for filtering genes
 ){
   geneid <- c()
   X.list <- lapply(esets, function(eset){
     return(t(exprs(eset)))
-  })
+  }) 
+
   index <- 1
   for(k in 1:ncol(X.list[[1]])){
-    cor.list <- c()
-    m <- 1
+    marker <- TRUE
     for(i in 1:length(esets)){
       for(j in i:length(esets)){
-        cor.list[m] <- cor(X.list[[i]][, k], X.list[[j]][, k])
-        m <- m + 1          
+        cor.mi <- cor(X.list[[i]])
+        cor.mj <- cor(X.list[[j]])
+        if(cor(cor.mi[k, ], cor.mj[k, ]) < cor.cutoff) marker <- FALSE
+        rm(cor.mi, cor.mj)
       }
-    }    
-    in.cor <- mean(cor.list)
-    if(in.cor > cor.cutoff){
+    }
+    if(marker) {
       geneid[index] <- k
       index <- index + 1
     }
   }
+  
   
   new.esets <- lapply(esets, function(eset){
     return(eset[geneid,])
@@ -35,12 +37,12 @@ geneFilter <- structure(function
 },ex=function(){
   library(curatedOvarianData)
   data( E.MTAB.386_eset )
-  eset1 <- E.MTAB.386_eset[, 1:5]
-  eset2 <- E.MTAB.386_eset[, 6:10]
-  eset3 <- E.MTAB.386_eset[, 11:15]  
+  eset1 <- E.MTAB.386_eset[1:500, 1:5]
+  eset2 <- E.MTAB.386_eset[1:500, 6:10]
+  eset3 <- E.MTAB.386_eset[1:500, 11:15]  
   esets <- list(eset1, eset2, eset3) 
   
-  result.set <- geneFilter(esets)
+  result.set <- geneFilter(esets, 0)
   result.set
   ### as we cannot calculate correlation with one set, this function just 
   ### delivers the same set if esets has length 1
